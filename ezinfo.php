525ad07a41d5cdaad1eec60163d7519b63ebb19d
Luca Realdi <lr@opencontent.it>
2016-01-17 10:58:41 +0100

    svn stable 2

--- ezinfo.php
+++ ezinfo.php
@@ -1,11 +1,11 @@
 <?php
-class sckcreatesubtreenoficationruleeventInfo
+class createsubtreenoficationruleeventInfo
 {
     function info()
     {
         return array(
-            'Name' => "Create subtree notification rule event type",
-            'Version' => "1.0",
+            'Name' => "<a href='http://projects.ez.no/createsubtreenotificationruleevent'>Create subtree notification rule event type</a>",
+            'Version' => "1.0.1",
             'Copyright' => "Copyright (C) 2006 SCK-CEN",
             'Author' => "Kristof Coomans",
             'License' => "GNU General Public License v2.0"
